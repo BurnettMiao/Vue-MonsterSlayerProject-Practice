3365951692d9d00454e36bc0f1d110eb2250d3c5
BurnettMiao <searchfor1987@gmail.com>
2023-09-05 09:50:05 +0800

    add special attack funciton

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
@@ -18,9 +19,13 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + '%' };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       // this.monsterHealth = this.monsterHealth - attackValue;
       this.monsterHealth -= attackValue;
@@ -29,6 +34,12 @@ const app = Vue.createApp({
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
   },
 });
