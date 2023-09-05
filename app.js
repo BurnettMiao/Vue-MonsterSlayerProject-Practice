c267fd4f419e7f088a6886f7c08d0397c5546fde
BurnettMiao <searchfor1987@gmail.com>
2023-09-05 09:25:11 +0800

    updating the health bar

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+console.log(getRandomValue(5, 12));
+
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
       monsterHealth: 100,
     };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + '%' };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + '%' };
+    },
   },
   methods: {
     attackMonster() {
